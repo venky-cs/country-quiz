c5728f43b98d99c9724e0fdf06f16071343ed2eb
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-02-16 23:38:20 +0530

    create-heading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+import logo from "./undraw_adventure_4hum 1.svg";
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <div className="main">
-        
+        <div className="heading">
+          <h3 className="title">country quiz</h3>
+          <img src={logo} alt="logo" />
+        </div>
       </div>
     </div>
   );
