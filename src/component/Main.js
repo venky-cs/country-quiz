5f74ba62dd6a0ad0d892016004d758d554e25e9b
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-02-18 23:11:58 +0530

    'get-random-data'

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,14 +1,16 @@
-import React,{useEffect} from 'react'
-import Axios from 'axios'
+import React,{useState,useEffect} from 'react'
 import axios from 'axios'
 
 
 function Main() {
+    const [state, setState] = useState([])
+    let randomNumber = Math.floor(Math.random(5)*250)
     useEffect(() => {
         axios
         .get("https://restcountries.eu/rest/v2/all")
         .then((res) => {
-            console.log(res.data)
+            console.log(res.data[randomNumber])
+            setState(res.data[randomNumber])
         })
     }, [])
     return (
