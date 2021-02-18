e3e51d3b5e67ba00e74f09f1d0f5acbc192167b0
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-02-18 23:41:07 +0530

    'create-layout'

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,23 +1,28 @@
-import React,{useState,useEffect} from 'react'
-import axios from 'axios'
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Main() {
-    const [state, setState] = useState([])
-    let randomNumber = Math.floor(Math.random(5)*250)
-    useEffect(() => {
-        axios
-        .get("https://restcountries.eu/rest/v2/all")
-        .then((res) => {
-            console.log(res.data[randomNumber])
-            setState(res.data[randomNumber])
-        })
-    }, [])
-    return (
-        <div>
-            
-        </div>
-    )
+  const [state, setState] = useState([]);
+  const [country, setCountry] = useState({});
+  let randomNumber = Math.floor(Math.random(5) * 200);
+  useEffect(() => {
+    axios.get("https://restcountries.eu/rest/v2/all").then((res) => {
+      setState(res.data);
+      setCountry(res.data[randomNumber]);
+    });
+  }, []);
+  return (
+    <div>
+      {console.log(state, country)}
+      <h3>{country.capital} is the capital of</h3>
+      <ul>
+        <li>{state.length >1 && state[randomNumber+1].name}</li>
+        <li>{state.length >1 && state[randomNumber+10].name}</li>
+        <li>{country.name}</li>
+        <li>{state.length >1 && state[randomNumber+20].name}</li>
+      </ul>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
